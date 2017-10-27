e803f79010e953bc3c5490a873df62631efd994d
Семенова Юлия Вадимовна <C:\Users\\AppData\Roaming\The Bat!>
2017-10-27 15:31:46 +0300

    merge

--- js/dialog.js
+++ js/dialog.js
@@ -72,50 +72,5 @@
 
 		document.addEventListener('mousemove', onMouseMove);
 		document.addEventListener('mouseup', onMouseUp);
-<<<<<<< HEAD
-
-	});
-
-	
-	var draggedItem = null;
-
-	shopElement.addEventListener('dragstart', function (evt) {
- 		if (evt.target.tagName.toLowerCase() === 'img') {
-    		draggedItem = evt.target.cloneNode(true);
-    		evt.dataTransfer.setData('text/plain', evt.target.alt);
-  		}
-  		artifactsElement.style.outline = '2px dashed red';
-	});
-
-	artifactsElement.addEventListener('dragover', function (evt) {
-		if (evt.target.draggable) {
-			evt.target.style.backgroundColor = 'red';
-		}
-		evt.preventDefault();
-		return false;
-	});
-
-	artifactsElement.addEventListener('drop', function (evt) {
-		  evt.target.style.backgroundColor = '';
-		  evt.target.appendChild(draggedItem);
-		  evt.preventDefault();
-		  artifactsElement.style.outline = '';
-	});
-
-	artifactsElement.addEventListener('dragenter', function (evt) {
-	  evt.target.style.backgroundColor = 'yellow';
-	  evt.preventDefault();
-	});
-
-	artifactsElement.addEventListener('dragleave', function (evt) {
-	  evt.target.style.backgroundColor = '';
-	  evt.preventDefault();
-	});
-=======
-
-	});
-
-	
->>>>>>> bd5b8d50038b5c791c69be91a0652d675048da00
 
 })();
